a1232084471a07e9e24703cc7b820381ea236ecb
zbtechph <zbtech@1fidel.tech>
2022-12-01 09:17:28 +0700

    add draw method

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express")
-//const seedrandom = require("seedrandom")
+const seedrandom = require("seedrandom")
 
-//const rng = seedrandom(seed)
+const rng = seedrandom(seed)
 
 const app = express()
+
+app.use(express.urlencoded({
+    extended: true
+}))
 
 app.use('/', express.static('public'))
 
 app.post('/execute', (req, res) => {
-    res.send("hello world?")
+    const { seed, prizeCount } = req.body
+    const entries = JSON.parse(req.body.entries)
+    const rng = seedrandom(seed)
+    const draw = entries.map(entry, ({name: entry, rand: rng()})).sort((a,b)=>a.rand-b.rand)
+    res.send(JSON.stringify(draw.splice(0,prizeCount)))
 });
 
 
