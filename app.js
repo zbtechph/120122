2c764b2b62fe62a0729b6288bdb10bd5b9fea046
zbtechph <zbtech@1fidel.tech>
2022-12-01 08:23:28 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const seedrandom = require("seedrandom")
+
+const rng = seedrandom(seed)
+
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req)
+    res.send("hello world?")
+});
+
+
+app.listen(4749, () => {
+    console.log("listening on port 4749")
+})
