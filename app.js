14b0bf32a68dd2fa616380ac30525da9c4ae60a0
zbtechph <66845323+zbtechph@users.noreply.github.com>
2022-12-01 22:05:34 +0800

    env.PORT must be uppercase

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const seedrandom = require("seedrandom")
 const sha256 = require('js-sha256')
-const port = process.env.port ?? 3000
+const port = process.env.PORT ?? 3000
 
 const app = express()
 
